0672d6fb34bc35535f61ee548feec3d0c140f46f
Tirsh <valeriy.vasipov@gmail.com>
2023-02-28 18:16:16 +0300

    HW04-1,2

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,45 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+
+@NamedQueries({
+@NamedQuery(name = Meal.GET_ALL, query = "SELECT m FROM Meal m " +
+        "WHERE m.user.id=:userId ORDER BY m.dateTime DESC"),
+@NamedQuery(name = Meal.GET_BETWEEN, query = "SELECT m FROM Meal m " +
+        "WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC"),
+@NamedQuery(name = Meal.GET, query = "SELECT m FROM Meal m LEFT JOIN FETCH m.user " +
+        "WHERE m.id=:id AND m.user.id=:userId"),
+@NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId"),
+@NamedQuery(name = Meal.UPDATE, query = "UPDATE Meal m SET m.dateTime=:dateTime," +
+        "m.description=:description, m.calories=:calories WHERE m.id=:id AND m.user.id=:userId")
+})
+@Entity
+@Table(name = "meal")
 public class Meal extends AbstractBaseEntity {
+    public static final String GET_ALL = "Meal.getAll";
+    public static final String GET_BETWEEN = "Meal.getBetween";
+    public static final String GET = "Meal.get";
+    public static final String DELETE = "Meal.delete";
+    public static final String UPDATE = "Meal.update";
+    @Column(name = "date_time", nullable = false, columnDefinition = "timestamp default now()")
+    @NotBlank
     private LocalDateTime dateTime;
 
+    @Column(name = "description")
+    @NotBlank
+    @Size(max = 128)
     private String description;
 
+    @Column(name = "calories", nullable = false, columnDefinition = "int default 2000")
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
